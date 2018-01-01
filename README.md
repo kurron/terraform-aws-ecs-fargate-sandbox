8ef57fe7635ddef179dd3bb02f8497eb4fb4a734
Ron Kurr <kurr@kurron.org>
2018-01-01 18:18:43 -0500

    Still trying to get the container to actually come up!

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Overview
 This Terraform module creates a sandbox with an [ALB](https://aws.amazon.com/elasticloadbalancing/) fronting an [ECS](https://aws.amazon.com/ecs/) cluster with [Fargate](https://aws.amazon.com/fargate/) launched containers.
 
+**NOTE:** this isn't working yet.  I don't get errors during deployment but the task never fully comes up and nothing gets sent to the logs.  Given that EC2 instances are no longer involved, I can't SSH into the box and poke around.   Currently, I've tried disconnecting the service from the ALB so see if I can get further but no joy.  My guess is that there is either a missing resource I need to create or there is an issue with the IAM Role.
+
 # Prerequisites
 * [Terraform](https://terraform.io/) installed and working
 * Development and testing was done on [Ubuntu Linux](http://www.ubuntu.com/)
@@ -23,5 +25,6 @@ then run `debug/debug-module.sh` to test your changes.
 
 # License and Credits
 This project is licensed under the [Apache License Version 2.0, January 2004](http://www.apache.org/licenses/).
+[Deploying FARGATE services using CloudFormation: The guide I wish I had](https://blog.devopspro.co.uk/deploying-fargate-services-using-cloudformation-the-guide-i-wish-i-had-d89b6dc62303)
 
 # List of Changes
--- debug/plan.tf
+++ debug/plan.tf
@@ -5,7 +5,7 @@ terraform {
 
 variable "region" {
     type = "string"
-    default = "us-west-2"
+    default = "us-east-1"
 }
 
 variable "project" {
@@ -30,15 +30,20 @@ variable "domain_name" {
 
 variable "vpc_id" {
     type = "string"
-    default = "vpc-ff217399"
+    default = "vpc-b85b93c0"
 }
 
 variable "subnet_ids" {
-    default = ["subnet-568ee830","subnet-4a33b402","subnet-ac5f72f7"]
+    default = [" subnet-d1eefc8b","subnet-7d229919"]
 }
 
 provider "aws" {
     region = "${var.region}"
+}
+
+variable "container_port" {
+    type = "string"
+    default = "80"
 }
 
 data "aws_acm_certificate" "certificate" {
@@ -57,7 +62,7 @@ module "alb" {
     environment        = "${var.environment}"
     freetext           = "No notes at this time."
     internal           = "No"
-    security_group_ids = ["sg-18d8b765"]
+    security_group_ids = ["sg-c161d4b3"]
     subnet_ids         = "${var.subnet_ids}"
     vpc_id             = "${var.vpc_id}"
     ssl_policy         = "ELBSecurityPolicy-TLS-1-2-2017-01"
@@ -73,7 +78,71 @@ resource "aws_ecs_cluster" "main" {
 }
 
 resource "aws_ecs_task_definition" "definition" {
-    family                = "Nginx"
-    container_definitions = "${file("debug/files/task-definition.json")}"
-    network_mode          = "bridge"
+    family                   = "Nginx"
+    container_definitions    = "${file("debug/files/task-definition.json")}"
+    network_mode             = "awsvpc"
+    cpu                      = "256"
+    memory                   = "512"
+    requires_compatibilities = ["FARGATE"]
+    execution_role_arn       = "arn:aws:iam::037083514056:role/ecs-role20171016145604531200000004"
+}
+
+resource "aws_ecs_service" "service" {
+    name                               = "Fargate"
+    task_definition                    = "${aws_ecs_task_definition.definition.arn}"
+    desired_count                      = "1"
+    cluster                            = "${aws_ecs_cluster.main.arn}"
+    deployment_maximum_percent         = "200"
+    deployment_minimum_healthy_percent = "50"
+
+    launch_type                        = "FARGATE"
+    network_configuration = {
+        subnets         = "${var.subnet_ids}"
+        security_groups = ["sg-8618d0f2"]
+    }
+}
+
+resource "aws_lb_target_group" "target_group" {
+    name_prefix          = "fgate-"
+    port                 = "${var.container_port}"
+    protocol             = "HTTP"
+    vpc_id               = "${var.vpc_id}"
+    target_type          = "ip"
+    deregistration_delay = 300
+    stickiness {
+        type            = "lb_cookie"
+        cookie_duration = 86400
+        enabled         = "false"
+    }
+    health_check {
+        interval            = "15"
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = "5"
+        healthy_threshold   = "5"
+        unhealthy_threshold = "2"
+        matcher             = "200-299"
+    }
+    tags {
+        Name        = "Fargate"
+        Project     = "${var.project}"
+        Purpose     = "Maps the ALB to the back end containers"
+        Creator     = "${var.creator}"
+        Environment = "${var.environment}"
+        Freetext    = "No notes yet"
+    }
+}
+
+resource "aws_lb_listener_rule" "insecure_rule" {
+    listener_arn = "${module.alb.insecure_listener_arn}"
+    priority     = "1"
+    action = {
+        target_group_arn = "${aws_lb_target_group.target_group.arn}"
+        type             = "forward"
+    }
+    condition = {
+        field = "path-pattern"
+        values = ["/*"]
+    }
 }
